e62868ae9669c9fa98c653c9d24be98f68088373
rafael <rafa.v_s@hotmail.com>
2024-12-13 16:35:57 -0300

    ad

--- rg/main.tf
+++ rg/main.tf
@@ -1,4 +1,33 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.name
-  location = var.location
+# Create a new resource group if rg_novo is true
+resource "azurerm_resource_group" "rg_hub_dev" {
+  for_each = var.rg_novo ? { "new" = "${var.rg_name}-${var.environment}" } : {}
+  name     = each.value
+  location = var.rg_location
+}
+
+# Reference an existing resource group if rg_existente is true
+data "azurerm_resource_group" "rg_data_dev" {
+  for_each = var.rg_existente ? { "existing" = "${var.rg_name}-${var.environment}" } : {}
+  name     = each.value
+}
+
+# Determine the resource group name and location dynamically
+locals {
+  resource_group_name = var.rg_novo ? azurerm_resource_group.rg_hub_dev["new"].name : data.azurerm_resource_group.rg_data_dev["existing"].name
+  resource_group_location = var.rg_novo ? azurerm_resource_group.rg_hub_dev["new"].location : data.azurerm_resource_group.rg_data_dev["existing"].location
+}
+
+# Use the determined resource group for dependent resources
+resource "azurerm_virtual_network" "vnet_spoke" {
+  name                = "vnet-spsoke"
+  location            = local.resource_group_location
+  resource_group_name = local.resource_group_name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "snet_spoke" {
+  name                 = "subnet-spoke"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet_spoke.name
+  address_prefixes     = ["10.1.0.0/24"]
 }
--- rg/variables.tf
+++ rg/variables.tf
@@ -1,7 +1,38 @@
-variables "name" {
+variable "environment" {
     type = string
+    default = null
 }
 
-variables "location" {
+variable "client_id" {
     type = string
+    default = null
+}
+
+variable "workspaces" {
+    type = string
+    default = null
+}
+
+variable "rg_novo" {
+  description = "Indicates whether to create a new resource group"
+  type        = bool
+  default     = true
+}
+
+variable "rg_existente" {
+  description = "Indicates whether to use an existing resource group"
+  type        = bool
+  default     = false
+}
+
+variable "rg_name" {
+  description = "Base name for the resource group"
+  type        = string
+  default     = "rg-actions"
+}
+
+variable "rg_location" {
+  description = "Azure region for the resource group"
+  type        = string
+  default     = "eastus"
 }
