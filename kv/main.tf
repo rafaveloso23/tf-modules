976b04e65d7a7f4ed17b66efe54cd5147a2137a7
rafael <rafa.v_s@hotmail.com>
2024-12-16 12:21:36 -0300

    ad

--- kv/main.tf
+++ kv/main.tf
@@ -13,7 +13,7 @@ data "azurerm_resource_group" "rg_data" {
 
 # Determine the resource group name and location dynamically
 locals {
-  resource_group_name = var.rg_novo ? azurerm_resource_group.kv_hub["new"].name : data.azurerm_resource_group.rg_data["existing"].name
+  resource_group_name = var.rg_novo ? azurerm_resource_group.rg_hub["new"].name : data.azurerm_resource_group.rg_data["existing"].name
   resource_group_location = var.rg_novo ? azurerm_resource_group.rg_hub["new"].location : data.azurerm_resource_group.rg_data["existing"].location
 }
 
