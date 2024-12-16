08de90084800c823e2a6044ff81a53dbe51af7d4
rafael <rafa.v_s@hotmail.com>
2024-12-16 12:31:48 -0300

    a

--- kv/variables.tf
+++ kv/variables.tf
@@ -3,26 +3,12 @@ variable "environment" {
     default = null
 }
 
-variable "rg_novo" {
-  description = "Indicates whether to create a new resource group"
-  type        = bool
-  default     = true
-}
-
-variable "rg_existente" {
-  description = "Indicates whether to use an existing resource group"
-  type        = bool
-  default     = false
-}
-
 variable "rg_name" {
-  description = "Base name for the resource group"
+  description = "Indicates whether to create a new resource group"
   type        = string
-  default     = "rg-actions"
 }
 
 variable "rg_location" {
-  description = "Azure region for the resource group"
+  description = "Base name for the resource group"
   type        = string
-  default     = "eastus"
 }
