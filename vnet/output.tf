de914db2b1c3ccfacee0054b0f90befe7922fa62
rafael <rafa.v_s@hotmail.com>
2024-12-16 12:55:14 -0300

    a

--- vnet/output.tf
+++
@@ -1,3 +0,0 @@
-# output "existing_rg_name" {
-#   value = try(data.azurerm_resource_group.rg_data_dev["existing"].name, null)
-# }
--- vnet/variables.tf
+++ vnet/variables.tf
@@ -1,20 +1,3 @@
-variable "environment" {
-    type = string
-    default = null
-}
-
-variable "rg_novo" {
-  description = "Indicates whether to create a new resource group"
-  type        = bool
-  default     = true
-}
-
-variable "rg_existente" {
-  description = "Indicates whether to use an existing resource group"
-  type        = bool
-  default     = false
-}
-
 variable "rg_name" {
   description = "Base name for the resource group"
   type        = string
