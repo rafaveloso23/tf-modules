79568755f8b585eea37d51a86da2fe7d9493297a
rafael <rafa.v_s@hotmail.com>
2024-12-30 17:27:30 -0300

    a

--- kv/access_policy.tf
+++ kv/access_policy.tf
@@ -1,22 +1,19 @@
-data "azurerm_key_vault" "acces_policy" {
-  name                = var.name
-  resource_group_name = var.rg_name
-}
-
 output "acces_policy" {
-  value = data.azurerm_key_vault.kv.access_policy
+  value = var.kv_existente ? data.azurerm_key_vault.kv["existing"].access_policy : []
 }
 
 output "object_ids" {
-  value = [for policy in data.azurerm_key_vault.kv.access_policy : policy.object_id]
+  value = var.kv_existente ? [for policy in data.azurerm_key_vault.kv["existing"].access_policy : policy.object_id] : []
 }
 
 locals {
-  is_object_id_in_list = contains(
-    [for policy in data.azurerm_key_vault.kv.access_policy : policy.object_id],
+  is_object_id_in_list = var.kv_existente && contains(
+    [for policy in data.azurerm_key_vault.kv["existing"].access_policy : policy.object_id],
     data.azurerm_client_config.current.object_id
   )
 }
+
+
 
 output "is_object_id_in_list" {
   value = local.is_object_id_in_list
