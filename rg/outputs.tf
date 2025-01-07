20e96611c79868af1e3f85ada7fb4c27a37fefd1
rafael <rafa.v_s@hotmail.com>
2025-01-07 20:45:20 -0300

    a

--- rg/outputs.tf
+++ rg/outputs.tf
@@ -3,6 +3,11 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg_hub.name
 }
 
+output "id" {
+  description = "The name of the resource group created or retrieved."
+  value       = azurerm_resource_group.rg_hub.id
+}
+
 output "resource_group_location" {
   description = "The location of the resource group created or retrieved."
   value       = azurerm_resource_group.rg_hub.location
