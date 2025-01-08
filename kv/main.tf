f31af276d62eb268fd2612d9bff023f10123cd98
rafael <rafa.v_s@hotmail.com>
2025-01-08 20:31:03 -0300

    a

--- kv/main.tf
+++ kv/main.tf
@@ -9,18 +9,18 @@ resource "azurerm_key_vault" "kv" {
   sku_name                    = var.sku_name
 }
 
-resource "azurerm_key_vault_access_policy" "example" {
-  for_each = var.access_policies != null ? var.access_policies : {}
+# resource "azurerm_key_vault_access_policy" "example" {
+#   for_each = var.access_policies != null ? var.access_policies : {}
 
-  key_vault_id            = azurerm_key_vault.kv.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = each.value.object_id
-  key_permissions         = each.value.key_permissions
-  secret_permissions      = each.value.secret_permissions
-  certificate_permissions = each.value.certificate_permissions
+#   key_vault_id            = azurerm_key_vault.kv.id
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = each.value.object_id
+#   key_permissions         = each.value.key_permissions
+#   secret_permissions      = each.value.secret_permissions
+#   certificate_permissions = each.value.certificate_permissions
 
-  depends_on = [azurerm_key_vault_access_policy.current]
-}
+#   depends_on = [azurerm_key_vault_access_policy.current]
+# }
 
 # resource "azurerm_key_vault_access_policy" "current" {
 #   for_each = var.kv_novo || var.kv_existente ? { "default" = local.key_vault_name } : {}
