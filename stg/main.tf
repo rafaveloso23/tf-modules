15c2f14fdb4aa0a99562f0571af82701a63caad6
rafael <rafa.v_s@hotmail.com>
2025-01-13 11:14:55 -0300

    a

--- stg/main.tf
+++ stg/main.tf
@@ -3,16 +3,16 @@ resource "azurerm_resource_group" "example" {
   location = var.rg_location
 }
 
-# resource "azurerm_storage_account" "example" {
-#   name                     = "stgrvsjskdadfs"
-#   resource_group_name      = azurerm_resource_group.example.name
-#   location                 = azurerm_resource_group.example.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "example" {
+  name                     = "stgrvsjskdadfs"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# resource "azurerm_storage_container" "example" {
-#   name                  = "vhds"
-#   storage_account_id    = azurerm_storage_account.example.id
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "example" {
+  name                  = "vhds"
+  storage_account_id    = azurerm_storage_account.example.id
+  container_access_type = "private"
+}
