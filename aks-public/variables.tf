aefff15e89e31fe0aaf884e1a525f004f9174f4d
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-02-18 23:45:12 -0300

    a

--- aks-public/variables.tf
+++ aks-public/variables.tf
@@ -7,6 +7,6 @@ variable "cluster_name" {
   type    = string
 }
 
-locals {
-  aks_cluster_name = var.internet_access ? "aks-cluster-02" : "aks-cluster-01"
-}
+# locals {
+#   aks_cluster_name = var.internet_access ? "aks-cluster-02" : "aks-cluster-01"
+# }
